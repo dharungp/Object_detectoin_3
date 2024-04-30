8e1cd99fd77897fc0d1f753726581a12ec0602d9
dharungp <99897041+dharungp@users.noreply.github.com>
2024-04-30 14:28:45 +0530

    Add files via upload

---
+++ flower.js
@@ -0,0 +1,44 @@
+img="";
+status=""
+function Back(){
+    window.location="index.html"
+}
+function preload(){
+img = loadImage("flower.jpg");
+}
+
+function setup(){
+    canvas = createCanvas(500,500)
+    canvas.position(315,200);
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detection Object";
+
+    }
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResults);
+    
+    }
+function gotResults(error,results){
+    if(error){
+        console.log(error)
+    }
+    console.log(results)
+    objects = results;
+    }
+function draw(){
+    image(img,0,0,500,500)
+    if(status != ""){
+        for(i = 0;i<objects.length;i++){
+            document.getElementById("status").innerHTML = "Status: Object Detected";
+
+            fill();
+            percent = floor(objects[i].confidence * 100)
+            text(objects[i].label +" "+percent +"%" , objects[i].x +15,objects[i].y +15);
+            noFill();
+            stroke(r,g,b);
+            rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+            }
+        }
+    }
---
+++ main.js
@@ -0,0 +1,20 @@
+img=""
+function Bedroom(){
+    window.location="Bedroom.html"
+}
+
+function TvAc(){
+    window.location="TvAc.html"
+}
+
+function PLANT(){
+    window.location="plant area.html"
+}
+
+function FLOWER(){
+    window.location="flower.html"
+}
+
+function COMPUTER(){
+    window.location="Computer.html"
+}
